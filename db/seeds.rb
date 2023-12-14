fd0fc07bdaeb0c56bb94e9b13af110e7def975ae
Ben Carr <carrbens@gmail.com>
2023-12-14 12:24:38 -0800

    seed some address data for the rides and drivers tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+addresses = [
+  '7000 NE Airport Way, Portland, OR 97218',
+  '4001 SW Canyon Rd, Portland, OR 97221',
+  '1005 W Burnside St, Portland, OR 97209',
+  '1 N Center Ct St, Portland, OR 97227',
+  '1945 SE Water Ave, Portland, OR 97214'
+]
+
+addresses.permutation(2).each do |start_address, destination_address|
+  Ride.create!(start_address:, destination_address:)
+end
+
+driver_addresses = [
+  '111 SW 5th Ave, Portland, OR 97204',
+  '1300 SW 5th Ave, Portland, OR 97201',
+  '4012 SE 17th Ave, Portland, OR 97202',
+  '12000 SW 49th Ave, Portland, OR 97219',
+  '5000 N Willamette Blvd, Portland, OR 97203'
+]
+
+driver_addresses.each do |home_address|
+  Driver.create!(home_address:)
+end
